6f4b66670dbc0de354a718fee3e6374f242a7452
dsumigar <114698332+dsumigar@users.noreply.github.com>
2023-12-14 11:46:54 -0500

    Update and rename plip_zeroshot_eval.py to zeroshot_eval.py

--- scripts/zeroshot_eval.py
+++ scripts/zeroshot_eval.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
-# Replace 'your_file.csv' with the actual path to your CSV file
 csv_file_path = 'output_predictions.csv'
 
-# Read data from CSV into a DataFrame
 df = pd.read_csv(csv_file_path)
 
-# Create a mapping between the actual labels and predicted labels
 label_mapping = {
     'adi': 'adipose tissue',
     'back': 'background',
@@ -20,11 +17,9 @@ label_mapping = {
     'tum': 'colorectal adenocarcinoma epithelium'
 }
 
-# Convert labels and predicted labels to lowercase (assuming case-insensitive matching)
 df['Label'] = df['Label'].str.lower()
 df['Predicted_Label'] = df['Predicted_Label'].str.strip('""').str.lower()
 
-# Add a new column for matching
 df['Match'] = df['Label'].map(label_mapping) == df['Predicted_Label']
 
 # Calculate the percentage of matches
